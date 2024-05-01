aa4e0a82bb9d841757b702fa79a626f306e359b0
Sherlock-Xpf <sherlock.xpf@gmail.com>
2024-05-01 23:30:34 +0800

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+// 定义全局布局
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
